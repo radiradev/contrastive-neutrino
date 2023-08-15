87b00dca8d1031cf1ec186cffbab25c566745194
radiradev <radi.radev.uk@gmail.com>
2023-08-15 09:49:10 +0000

    Free up some morey by not loading training data twice

--- train.py
+++ train.py
@@ -7,13 +7,11 @@ import os
 import yaml
 from data_utilities.collation import clr_sparse_collate
 
-
 def load_yaml(path):
     with open(path, 'r') as f:
         return yaml.load(f, Loader=yaml.FullLoader)
     
 config = load_yaml('config/config.yaml')
-
 
 # Data
 dataset = CLRDataset(config['data']['data_path'])
@@ -23,9 +21,9 @@ train_dataset, val_dataset = random_split(dataset, lengths=lengths, generator=to
 train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, collate_fn=clr_sparse_collate, num_workers=2)
 val_dataloader = DataLoader(val_dataset, batch_size=128, shuffle=False, collate_fn=clr_sparse_collate)
 
-# test one batch 
-batch = next(iter(train_loader))
-xi, xj = batch
+# # test one batch 
+# batch = next(iter(train_loader))
+# xi, xj = batch
 
 model = SimCLR()
 
