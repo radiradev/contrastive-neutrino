a791403c7e14c5317c8689acfe5d1d48b6b584ad
radiradev <radi.radev.uk@gmail.com>
2024-01-30 13:10:34 +0000

    Updating logging logic according to lightning-template

--- modules/classifier.py
+++ modules/classifier.py
@@ -4,7 +4,6 @@ import pytorch_lightning as pl
 from MinkowskiEngine import SparseTensor
 import numpy as np
 import torchmetrics
-from torchmetrics.functional import accuracy
 
 
 class SingleParticleModel(pl.LightningModule):
@@ -12,9 +11,24 @@ class SingleParticleModel(pl.LightningModule):
         super().__init__()
         self.model = VoxelConvNeXtClassifier(in_chans=1, D=3, num_classes=5)
         self.loss = torch.nn.CrossEntropyLoss()
-        self.test_accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=5)
-        self.val_accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=5)
+
+        #metrics
+        self.train_acc = torchmetrics.Accuracy(task='multiclass', num_classes=5)
+        self.val_acc = torchmetrics.Accuracy(task='multiclass', num_classes=5)
+
+        #losses
+        self.train_loss = torchmetrics.MeanMetric()
+        self.val_loss = torchmetrics.MeanMetric()
+
+        #track best loss
+        self.val_loss_best = torchmetrics.MinMetric()
+
         self.batch_size = batch_size
+
+    def on_train_start(self) -> None:
+        "Lightning hook called before training, useful to initialize things"
+        self.val_loss.reset()
+        self.val_loss_best.reset()
 
     def forward(self, x):
         return self.model(x)
@@ -39,17 +53,36 @@ class SingleParticleModel(pl.LightningModule):
     
     def training_step(self, batch, batch_idx):
         loss, predictions, labels = self._shared_step(batch, batch_idx)
-        self.log('train_acc', accuracy(predictions, labels), on_step=True, on_epoch=False, prog_bar=True, batch_size=self.batch_size)
-        self.log('train_loss', loss, prog_bar=True, on_step=True, on_epoch=False, batch_size=self.batch_size)
+        
+        #update metrics
+        self.train_loss = loss
+        self.train_acc(predictions, labels)
+
+        #log metrics
+        self.log('train/loss', self.train_loss, prog_bar=True, on_step=True, on_epoch=True, batch_size=self.batch_size)
+        self.log('train/acc', self.train_acc, prog_bar=True, on_step=True, on_epoch=True, batch_size=self.batch_size)
+
         return loss
     
     def validation_step(self, batch, batch_idx):
         loss, predictions, labels = self._shared_step(batch, batch_idx)
-        self.val_accuracy(predictions, labels)
-        self.log('val_acc', self.val_accuracy,on_step=False, on_epoch=True, batch_size=self.batch_size)
-        self.log('val_loss', loss, on_step=False, on_epoch=True, batch_size=self.batch_size)
+
+        #update metrics
+        self.val_loss(loss)
+        self.val_acc(predictions, labels)
+
+        #log metrics
+        self.log('val/loss', self.val_loss, on_step=False, on_epoch=True, batch_size=self.batch_size)
+        self.log('val/acc', self.val_acc, on_step=False, on_epoch=True, batch_size=self.batch_size)
         return loss
 
+    def on_validation_epoch_end(self) -> None:
+        "Lightning hook that is called when a validation epoch ends."
+        loss = self.val_loss.compute()  # get current val acc
+        self.val_loss_best(loss)  # update best so far val acc
+        # log `val_acc_best` as a value through `.compute()` method, instead of as a metric object
+        # otherwise metric would be reset by lightning after each epoch
+        self.log("val/loss_best", self.val_loss_best.compute(), sync_dist=True, prog_bar=True)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
--- modules/simclr.py
+++ modules/simclr.py
@@ -8,21 +8,35 @@ from models.voxel_convnext import VoxelConvNeXtCLR
 
 
 class SimCLR(pl.LightningModule):
-    def __init__(self, batch_size=256, num_gpus=1, gather_distributed=True):
+    def __init__(self, batch_size=None, num_gpus=1, gather_distributed=True):
+        if batch_size is None:
+            raise ValueError("batch_size must be specified")
+        
         super().__init__()
         self.model = VoxelConvNeXtCLR(in_chans=1, D=3)
         self.criterion = contrastive_loss
         self.batch_size = batch_size
+
         # sometimes we might want to use multiple gpus but a smaller efective batch_size
         if num_gpus > 1 and gather_distributed:
             self.gather_distributed = True
         else: 
             self.gather_distributed = False
+
+        #losses
+        self.train_loss = torchmetrics.MeanMetric()
+        self.val_loss = torchmetrics.MeanMetric()
+        self.val_loss_best = torchmetrics.MinMetric()
+
     def forward(self, x):
         return self.model(x)
+
+    def on_train_start(self) -> None:
+        "Lightning hook called before training, useful to initialize things"
+        self.val_loss.reset()
+        self.val_loss_best.reset()
     
     def _create_tensor(self, features, coordinates):
-
         return SparseTensor(
             features=features.float(),
             coordinates=coordinates,
@@ -41,13 +55,29 @@ class SimCLR(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         # Must clear cache at regular interval
         loss = self._shared_step(batch, batch_idx)
-        self.log('train_loss', loss, prog_bar=True, batch_size=self.batch_size, on_step=True, on_epoch=False)
+
+        #compute loss and log it
+        self.train_loss(loss)
+        self.log('train/loss', self.train_loss, prog_bar=True, batch_size=self.batch_size, on_step=True, on_epoch=True)
+
         return loss
     
     def validation_step(self, batch, batch_idx):
         loss = self._shared_step(batch, batch_idx)
-        self.log('val_loss', loss, batch_size=self.batch_size, on_epoch=True, on_step=False)
+        
+        #compute loss and log it
+        self.val_loss(loss)
+        self.log('val/loss', self.val_loss, batch_size=self.batch_size, on_step=False, on_epoch=True)
+
         return loss
+
+    def on_validation_epoch_end(self) -> None:
+        "Lightning hook that is called when a validation epoch ends."
+        loss = self.val_loss.compute()  # get current val acc
+        self.val_loss_best(loss)  # update best so far val acc
+        # log `val_acc_best` as a value through `.compute()` method, instead of as a metric object
+        # otherwise metric would be reset by lightning after each epoch
+        self.log("val/loss_best", self.val_loss_best.compute(), sync_dist=True, prog_bar=True)    
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
