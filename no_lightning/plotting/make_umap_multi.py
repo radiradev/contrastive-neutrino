9d32ca5a768c7b354f156c7d5fa4ace360b4bf29
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-06-24 20:21:16 +0100

    plotting

--- no_lightning/plotting/make_umap_multi.py
+++ no_lightning/plotting/make_umap_multi.py
@@ -11,29 +11,33 @@ matplotlib.rcParams['axes.prop_cycle'] = matplotlib.cycler(color=['#377eb8', '#f
 LABELS = { 0 : "electron", 1 : "gamma", 2 : "muon", 3 : "pion", 4 : "proton" }
 
 def main(args):
+    embedding_clf_nominal, labels_clf_nominal = get_umap_embedding(args.preds_clf_nominal)
     embedding_clf, labels_clf = get_umap_embedding(args.preds_clf)
     embedding_clr, labels_clr = get_umap_embedding(args.preds_clr)
     embedding_dann, labels_dann = get_umap_embedding(args.preds_dann)
 
-    fig, ax = plt.subplots(1, 3, figsize=(10, 5))
+    fig, ax = plt.subplots(1, 4, figsize=(12, 5))
     for label_num, name in LABELS.items():
+        embedding_label_clf_nominal = embedding_clf_nominal[(labels_clf_nominal == label_num)]
+        ax[0].scatter(embedding_label_clf_nominal[:, 0], embedding_label_clf_nominal[:, 1], label=name, s=1)
         embedding_label_clf = embedding_clf[(labels_clf == label_num)]
-        ax[0].scatter(embedding_label_clf[:, 0], embedding_label_clf[:, 1], label=name, s=1)
+        ax[1].scatter(embedding_label_clf[:, 0], embedding_label_clf[:, 1], label=name, s=1)
         embedding_label_clr = embedding_clr[(labels_clr == label_num)]
-        ax[1].scatter(embedding_label_clr[:, 0], embedding_label_clr[:, 1], label=name, s=1)
+        ax[2].scatter(embedding_label_clr[:, 0], embedding_label_clr[:, 1], label=name, s=1)
         embedding_label_dann = embedding_dann[(labels_dann == label_num)]
-        ax[2].scatter(embedding_label_dann[:, 0], embedding_label_dann[:, 1], label=name, s=1)
+        ax[3].scatter(embedding_label_dann[:, 0], embedding_label_dann[:, 1], label=name, s=1)
 
     for a in ax:
         a.set_xticks([])
         a.set_yticks([])
-    ax[0].set_title("Classifier", fontsize=12)
-    ax[1].set_title("CLR", fontsize=12)
-    ax[2].set_title("DANN", fontsize=12)
+    ax[0].set_title("Classifier Nominal", fontsize=12)
+    ax[1].set_title("Classifier Throw", fontsize=12)
+    ax[2].set_title("Contrastive Pretraining Throw", fontsize=12)
+    ax[3].set_title("DANN Throw", fontsize=12)
     plt.legend(loc="upper right", fontsize=10, markerscale=3)
     fig.suptitle(args.title, fontsize=14)
     fig.tight_layout()
-    plt.savefig("umap_triplet.pdf")
+    plt.savefig("umap_multi.pdf")
     plt.close()
     # plt.show()
 
@@ -49,6 +53,7 @@ def get_umap_embedding(preds_path):
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("preds_clf_nominal")
     parser.add_argument("preds_clf")
     parser.add_argument("preds_clr")
     parser.add_argument("preds_dann")
