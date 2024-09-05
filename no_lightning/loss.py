8ee445629c0cf09b68ed708d46788112845ca394
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-09-05 16:32:06 +0100

    implement supervised contrastive loss as in paper

--- no_lightning/loss.py
+++ no_lightning/loss.py
@@ -110,6 +110,67 @@ def contrastive_loss_class_labels(
 
     return loss
 
+def contrastive_loss_class_labels_out(
+    x_i, x_j, labels, temperature=0.1, gather_distributed=False, same_label_weight=0.5
+):
+    """
+    Contrastive loss function from bmdillon/JetCLR
+
+    Uses class labels to define positive pairs.
+
+    Formulated in the same way as L^sup_out of eq 2 in arXiv:2004.11362.
+
+    Args:
+        x_i (torch.Tensor): Input tensor of shape (batch_size, n_features)
+        x_j (torch.Tensor): Input tensor of shape after augmentations (batch_size, n_features)
+        temperature (float, optional): Temperature parameter. Defaults to 0.1.
+    Returns:
+        torch.Tensor: Contrastive loss
+    """
+    eps = 1e-7
+
+    if gather_distributed and get_world_size() == 1:
+        raise ValueError("gather_distributed=True but number of processes is 1")
+
+    xdevice = x_i.get_device()
+
+    if gather_distributed:
+        x_i = torch.cat(GatherLayer.apply(x_i), dim=0)
+        x_j = torch.cat(GatherLayer.apply(x_j), dim=0)
+
+    batch_size = x_i.shape[0]
+    z_i = F.normalize(x_i, dim=1 )
+    z_j = F.normalize(x_j, dim=1 )
+    z = torch.cat( [z_i, z_j], dim=0 )
+    similarity_matrix = F.cosine_similarity( z.unsqueeze(1), z.unsqueeze(0), dim=2 )
+
+    # 0.5 for same class pairs, 1.0 for same image pairs
+    labels = torch.cat([labels, labels], dim=0)
+    print(labels)
+    print()
+    positives_mask = (labels[:, None] == labels[None, :]).float() * same_label_weight
+    ids = torch.cat([torch.arange(batch_size), torch.arange(batch_size)], dim=0)
+    positives_mask += (ids[:, None] == ids[None, :]).float() * (1.0 - same_label_weight)
+    positives_mask *= (~torch.eye(2 * batch_size, 2 * batch_size, dtype=bool)).float()
+    positives_mask = positives_mask.to(xdevice)
+    nominator = positives_mask * torch.exp(similarity_matrix / temperature)
+
+    # NOTE Do I need to include the same 0.5 weighting for the class pairs in the denominator too?
+    # If I dont the loss minimum is no longer zero, not sure if that is really going to matter.
+    # If I did I would need to tweak the negatives mask to be 0.5 for the class pairs
+    negatives_mask = ( ~torch.eye( 2*batch_size, 2*batch_size, dtype=bool ) ).float()
+    negatives_mask = negatives_mask.to( xdevice )
+    denominator = negatives_mask * torch.exp( similarity_matrix / temperature )
+
+    summand = nominator / torch.sum(denominator, dim=1)
+    summand = torch.where(summand < eps, 1.0, summand) # So the log doesnt change the zeros
+    summand = torch.log(summand)
+    positive_pair_cardinality = torch.count_nonzero(summand, dim=1)
+    loss_partial = torch.sum(summand, dim=1)
+    loss = torch.sum((-1 / positive_pair_cardinality) * loss_partial) / (2 * batch_size)
+
+    return loss
+
 class NT_Xent(nn.Module):
     def __init__(self, batch_size, world_size, temperature=0.1):
         super(NT_Xent, self).__init__()
