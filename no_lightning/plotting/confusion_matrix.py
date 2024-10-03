de493a10b4a42078b6deee1849064a779741d203
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-10-03 12:47:31 +0100

    plotting

--- no_lightning/plotting/confusion_matrix.py
+++ no_lightning/plotting/confusion_matrix.py
@@ -14,6 +14,13 @@ matplotlib.rcParams['axes.prop_cycle'] = matplotlib.cycler(color=['#377eb8', '#f
 LABELS = { 0 : "electron", 1 : "gamma", 2 : "muon", 3 : "pion", 4 : "proton" }
 
 def main(args):
+    if args.labels == 0:
+        labels = { 0 : "electron", 1 : "gamma", 2 : "muon", 3 : "pion", 4 : "proton" }
+    elif args.labels == 1:
+        labels = { 0 : "electron", 1 : "muon", 2 : "pion", 3 : "proton" }
+    else:
+        raise ValueError(f"--labels={args.labels} not valid")
+
     with open(args.preds, "r") as f:
         pred_data = yaml.load(f, Loader=yaml.FullLoader)
     pred_label, true_label = [], []
@@ -25,16 +32,16 @@ def main(args):
 
     # group_counts = [ "{0:0.0f}".format(value) for value in C.flatten() ]
     group_percentages = [ "{0:.2%}".format(value) for value in (C / np.sum(C, axis=1)).flatten() ]
-    labels = group_percentages
-    labels = np.asarray(labels).reshape(5,5)
+    element_labels = group_percentages
+    element_labels = np.asarray(element_labels).reshape(len(labels), len(labels))
     sns.set(font_scale=1.4)
     fig, ax = plt.subplots(figsize=(8,7))
-    sns.heatmap(C, annot=labels, annot_kws={'size': 16}, fmt='', ax=ax, cmap='Blues')
+    sns.heatmap(C, annot=element_labels, annot_kws={'size': 16}, fmt='', ax=ax, cmap='Blues')
     ax.set_xlabel("Predicted labels")
     ax.set_ylabel("True labels")
     ax.set_title(args.title)
-    ax.xaxis.set_ticklabels([ LABELS[i] for i in range(5) ])
-    ax.yaxis.set_ticklabels([ LABELS[i] for i in range(5) ])
+    ax.xaxis.set_ticklabels([ label for label in labels.values() ])
+    ax.yaxis.set_ticklabels([ label for label in labels.values() ])
     fig.tight_layout()
     plt.savefig("confusion_matrix.pdf")
     plt.close()
@@ -42,8 +49,9 @@ def main(args):
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("preds")
-    parser.add_argument("title")
+    parser.add_argument("preds", type=str)
+    parser.add_argument("title", type=str)
+    parser.add_argument("labels", type=int, help="0|1")
 
     args = parser.parse_args()
 
